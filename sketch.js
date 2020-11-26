76bb7d8033f57208dfe0549353ab6418124c374c
Kshitij542 <71619725+Kshitij542@users.noreply.github.com>
2020-11-26 22:17:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,61 +1,70 @@
-var ball1,ground,b1,b2,b3;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var dimg,dsprite;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-function preload()
-{
-	dimg=loadImage("dustbingreen.png");
-	
-}
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
-	ground=new Ground(400,690,800,20);
-	ball1=new Ball(60,200,20,40);
-	b1=new buckets(660,640,20,100,PI/2)
-	b2=new buckets(715,680,100,20,PI/4)
-	b3=new buckets(770,640,20,100,PI/2)
-	dsprite= createSprite(715,580,100,20);
-	dsprite.addImage(dimg);
-	dsprite.scale=0.6;
+	
+	paperObject=new paper(200,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	//Create a Ground
+	
 
-	//Create the Bodies Here.
-
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 
 	Engine.run(engine);
+	Render.run(render);
   
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background("skyblue");
-  ball1.display();
-  ground.display();
-  b1.display();
-  b2.display();
-  b3.display();
+  background(230);
+ 
+  
+  paperObject.display();
+  groundObject.display();
+  dustbinObj.display();
 
-
-  keypressed();
-
-  drawSprites();
+  
+  
+ 
+  
+  
  
 }
-function keypressed()
-{
-	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(ball1.body,ball1.body.position,{x:85,y:-85});
-	}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
+    
+  	}
 }
+
+
 
 
 
