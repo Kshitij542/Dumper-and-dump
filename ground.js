6ca7b2b00061a03b393c2735968d1e7efa18cd0a
Kshitij542 <71619725+Kshitij542@users.noreply.github.com>
2020-11-26 22:15:09 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,24 +1,31 @@
-class Ground{
-    constructor(x, y, width, height) {
-        var options = {
-            'restitution':0.0,
-            'friction':1.0,
-            'density':1.0,
-            'isStatic':true,
-             }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        World.add(world, this.body);
-      }
-      display(){
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        rectMode(CENTER);
-        fill(255,255,0);
-        rect(0,0,this.width,this.height);
-         pop();
-      }
+class ground
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
 }
